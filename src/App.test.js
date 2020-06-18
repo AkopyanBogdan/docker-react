fc3e2ad60787b447c41077c03085eb4632473035
akopyan.bogdan <akopyan.bogdan@gmail.com>
2020-06-18 18:05:15 +0300

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
+
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Changed on feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
 
